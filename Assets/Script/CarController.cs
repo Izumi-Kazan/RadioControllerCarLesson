050c7e0743665439ba6d80080bb625ffe291eae1
Izumi Kazan <hoge_no1@hotmail.com>
2015-06-22 18:10:10 +0900

    WheelCollider, Script追加
    うまくいかない…

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+	public WheelCollider wcFrontLeft;
+	public WheelCollider wcFrontRight;
+	public WheelCollider wcRearLeft;
+	public WheelCollider wcRearRight;
+
+	
+	public Vector3 centerOfMass;
+	public float enginePower = 30f;
+	public float brakePower = 10f;
+	public float steerAngleMax = 20f;
+	
+	private bool isAccelOn;
+	private bool isFoward;
+	private bool isBack;
+	
+	void Start () {
+		GetComponent<Rigidbody>().centerOfMass = centerOfMass;
+	}
+	
+	void FixedUpdate () {
+		
+		if (Input.GetKey(KeyCode.W) || isFoward) {
+			isAccelOn = true;
+			float torue = enginePower;
+			float torueIdle = enginePower * 0.2f;
+			wcFrontLeft.motorTorque = (wcFrontLeft.isGrounded) ? torue : torueIdle;
+			wcFrontRight.motorTorque = (wcFrontRight.isGrounded) ? torue : torueIdle;
+			wcRearLeft.motorTorque = (wcRearLeft.isGrounded) ? torue : torueIdle;
+			wcRearRight.motorTorque = (wcRearRight.isGrounded) ? torue : torueIdle;
+			
+			Debug.Log("W");
+			
+		} else if (Input.GetKey(KeyCode.S) || isBack) {
+			isAccelOn = true;
+			float torue = enginePower * -0.8f;
+			float torueIdle = enginePower * -0.2f;
+			wcFrontLeft.motorTorque = (wcFrontLeft.isGrounded) ? torue : torueIdle;
+			wcFrontRight.motorTorque = (wcFrontRight.isGrounded) ? torue : torueIdle;
+			wcRearLeft.motorTorque = (wcRearLeft.isGrounded) ? torue : torueIdle;
+			wcRearRight.motorTorque = (wcRearRight.isGrounded) ? torue : torueIdle;
+		} else {
+			isAccelOn = false;
+			wcFrontLeft.motorTorque = 0;
+			wcFrontRight.motorTorque = 0;
+			wcRearLeft.motorTorque = 0;
+			wcRearRight.motorTorque = 0;
+			
+			wcFrontLeft.brakeTorque = brakePower;
+			wcFrontRight.brakeTorque = brakePower;
+			wcRearLeft.brakeTorque = brakePower;
+			wcRearRight.brakeTorque = brakePower;
+		}
+		
+		float h = Input.GetAxis("Horizontal") + -Input.acceleration.y;
+		float angle = h * steerAngleMax;
+		wcFrontLeft.steerAngle = angle;
+		wcFrontRight.steerAngle = angle;
+	}
+	
+	public bool IsAccelOn () {
+		return isAccelOn;
+	}
+	
+	public bool IsWheelGrounded () {
+		return wcFrontLeft.isGrounded || wcFrontRight.isGrounded 
+			|| wcRearLeft.isGrounded || wcRearRight.isGrounded;
+	}
+	
+	public float GetEngineRevolution () {
+		return Mathf.Abs(wcFrontLeft.rpm + wcFrontRight.rpm + wcRearLeft.rpm + wcRearRight.rpm) * 0.25f;
+	}
+	
+//  #if UNITY_IPHONE || UNITY_ANDROID
+//  	void OnGUI () {
+//  		isFoward = GUI.RepeatButton(new Rect(Screen.width - 80, Screen.height - 160, 80, 70), "GO");
+//  		isBack = GUI.RepeatButton(new Rect(Screen.width - 80, Screen.height - 80, 80, 70), "BACK");
+//  	}
+//  #endif
+}
---
+++ Assets/Script/Wheel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wheel : MonoBehaviour {
+
+	public WheelCollider wheelCollider;
+	
+	private float rotation;
+	private WheelHit hit;
+
+	void Update () {
+		Vector3 colliderCenter = wheelCollider.transform.TransformPoint(wheelCollider.center);
+		
+		if (wheelCollider.GetGroundHit(out hit)) {
+			transform.position = hit.point + (wheelCollider.transform.up * wheelCollider.radius);
+		} else {
+			transform.position = colliderCenter - (wheelCollider.transform.up * wheelCollider.suspensionDistance);
+		}
+		transform.rotation = wheelCollider.transform.rotation * Quaternion.Euler(rotation, wheelCollider.steerAngle, 0);
+		rotation += wheelCollider.rpm * (360 / 60) * Time.deltaTime;
+	}
+}
